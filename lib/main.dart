f5940ba4e4b0bb2a83f361f27c213f52ffc177dd
MD SHAKIL HOSSAAIN <mdshakilhossain720@gmail.com>
2025-01-21 15:16:42 +0600

    notifaction all

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+import 'utils/notifaction_handler.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await setupFlutterNotifications();
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+  firebaseMessagingForgroundHandler();
   runApp(const MyApp());
 }
 
