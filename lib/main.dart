22e9d45a62b5ed434c2a209f840b83c69df4e54a
MD SHAKIL HOSSAAIN <mdshakilhossain720@gmail.com>
2025-02-04 10:19:26 +0600

    hive

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,18 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
+import 'componements/hive_fuction.dart';
+import 'config/app_constants.dart';
 import 'firebase_options.dart';
 import 'utils/notifaction_handler.dart';
 import 'view/all_route_screen.dart';
-import 'view/auth/registration.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  //await Hive.openBox(HiveFunctions.userHiveBox);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -32,33 +36,32 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          inputDecorationTheme:InputDecorationTheme(
-      // floatingLabelBehavior: FloatingLabelBehavior.always,
-      isDense: false,
-      contentPadding: const EdgeInsets.all(15),
-      hintStyle: const TextStyle(
-          color:Colors.grey,
-          fontSize: 16,
-          fontWeight: FontWeight.w300),
-      filled: false,
-      fillColor: Colors.transparent,
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(12),
-        borderSide: BorderSide(color:Colors.deepPurple ),
-      ),
-      enabledBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(12),
-        borderSide: BorderSide(color: const Color.fromARGB(255, 207, 29, 29)),
-      ),
-      focusedBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(12),
-        borderSide: const BorderSide(color: Colors.deepPurple),
-      ),
-      errorBorder: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(12),
-        borderSide: const BorderSide(color: Colors.red),
-      ),
-        ),
+          inputDecorationTheme: InputDecorationTheme(
+            // floatingLabelBehavior: FloatingLabelBehavior.always,
+            isDense: false,
+            contentPadding: const EdgeInsets.all(15),
+            hintStyle: const TextStyle(
+                color: Colors.grey, fontSize: 16, fontWeight: FontWeight.w300),
+            filled: false,
+            fillColor: Colors.transparent,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide(color: Colors.deepPurple),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide:
+                  BorderSide(color: const Color.fromARGB(255, 207, 29, 29)),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: const BorderSide(color: Colors.deepPurple),
+            ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: const BorderSide(color: Colors.red),
+            ),
+          ),
           // This is the theme of your application.
           //
           // Try running your application with "flutter run". You'll see the
