b1fd2fa9cde7a27f433c7e129ef3bf1d773bcbca
MD SHAKIL HOSSAAIN <mdshakilhossain720@gmail.com>
2025-02-08 09:48:45 +0600

    controller regestrion

--- lib/controllers/common/auth.dart
+++ lib/controllers/common/auth.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mvvmapi/model/common/signup_creadntail.dart';
 import 'package:mvvmapi/services/auth_services.dart';
 import 'package:mvvmapi/services/base/signup_base.dart';
 
@@ -8,28 +9,53 @@ import '../../model/common/user.dart';
 class AuthController extends StateNotifier<bool> {
   final Ref ref;
   AuthController(this.ref) : super(false);
-  Future<CommonResponse>login({required String contract, required String password}) async {
+  Future<CommonResponse> login(
+      {required String contract, required String password}) async {
     state = true;
     bool isSuccess = false;
 
-    try{
-      final response=await ref.read(authServiceProvider).login(contract: contract, password: password);
+    try {
+      final response = await ref
+          .read(authServiceProvider)
+          .login(contract: contract, password: password);
       state = false;
-      if(response.statusCode == 200){
+      if (response.statusCode == 200) {
         isSuccess = true;
-        final userInfo=User.fromMap(response.data);
-        final authToken=response.data['data']['token'];
-        
-      }return CommonResponse(isSuccess: isSuccess, message: response.data['message']);
-
-    }catch(e){
-     
+        final userInfo = User.fromMap(response.data);
+        final authToken = response.data['data']['token'];
+      }
+      return CommonResponse(
+          isSuccess: isSuccess, message: response.data['message']);
+    } catch (e) {
       state = false;
       return CommonResponse(isSuccess: false, message: e.toString());
-    }finally{
+    } finally {
       state = false;
     }
   }
 
-  
+  // regestion
+
+  Future<CommonResponse> signUp(SignupCreadntail signupCreadntail) async {
+    state = true;
+    bool isSuccess = false;
+
+    try {
+      final response=await ref.read(authServiceProvider).signup(signupCreadntail: signupCreadntail);
+      state =false;
+      if(response.statusCode==200){
+        isSuccess=true;
+        final userInfo=User.fromMap(response.data['data'] ['user']);
+        final userToken=response.data['data']['token'];
+
+
+      }
+      return CommonResponse(isSuccess: isSuccess, message: response.data['message']);
+    } catch (e) {
+      state = false;
+      return CommonResponse(isSuccess: false, message: e.toString());
+    } finally {
+      state = false;
+    }
+  }
 }
